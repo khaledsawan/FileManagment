8c8058a0c7899efc0eb9a3ab134c6a0bb9a3fdc7
khaledsawan <khaled963sawan@gmail.com>
2023-12-04 16:16:02 +0300

    group_user

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -8,11 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = ['name'];
 
     public function files()
     {
         return $this->hasMany(File::class);
+    }
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
